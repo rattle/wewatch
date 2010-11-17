f0b071e0edf34cdf4f03e355d18d80204c0bd66a
Tim Duckett <tim@timduckett.co.uk>
2010-11-17 13:58:00 +0000

    Basic homepage view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,35 @@ class PagesController < ApplicationController
 
   def index
       @title = "Home"
+      
+      today = Date.today
+      
+      start_time = DateTime.parse(today.strftime("%Y-%m-%d") + "T19:00:00")
+      end_time = DateTime.parse(today.strftime("%Y-%m-%d") + "T19:59:59")
+      
+      @broadcasts_7pm = Broadcast.all(:conditions => {:start => start_time..end_time, :is_repeat => false}, :order => "intentions_count DESC", :include => :channel)
+
+      start_time += 1.hour
+      end_time += 1.hour
+
+      @broadcasts_8pm = Broadcast.all(:conditions => {:start => start_time..end_time, :is_repeat => false}, :order => "intentions_count DESC", :include => :channel)
+
+      start_time += 1.hour
+      end_time += 1.hour
+
+      @broadcasts_9pm = Broadcast.all(:conditions => {:start => start_time..end_time, :is_repeat => false}, :order => "intentions_count DESC", :include => :channel)
+
+      start_time += 1.hour
+      end_time += 1.hour
+
+      @broadcasts_10pm = Broadcast.all(:conditions => {:start => start_time..end_time, :is_repeat => false}, :order => "intentions_count DESC", :include => :channel)
+
+      start_time += 1.hour
+      end_time += 1.hour
+
+      @broadcasts_11pm = Broadcast.all(:conditions => {:start => start_time..end_time, :is_repeat => false}, :order => "intentions_count DESC", :include => :channel)
+
+      
   end
 
   def about
--- app/models/broadcast.rb
+++ app/models/broadcast.rb
@@ -2,5 +2,10 @@ class Broadcast < ActiveRecord::Base
     
     belongs_to :channel
     has_many :intentions
+
+    scope :significant, :conditions => {:is_repeat => false}
+    scope :by_most_popular, :order => "intentions_count DESC"
+    
+
     
 end
