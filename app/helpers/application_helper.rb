501d78843b3aeb6366e0756fa87175dd71ea1d97
Frankie Roberto <frankie@frankieroberto.com>
2010-11-18 14:49:48 +0000

    Helper for displaying more readable durations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  
+  def hours_and_minutes(number)
+    hours = (number / 60).to_i
+    minutes = number.remainder(60)
+    
+    if hours > 0
+      if minutes > 0 
+        pluralize(hours, "hr") + ", " + pluralize(minutes, "min")
+      else
+        pluralize(hours, "hr")
+      end
+    else
+      pluralize(minutes, "min")
+    end
+  end
+  
+  
 end
