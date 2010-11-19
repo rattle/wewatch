411921bc973b6b41fd154d5138f254cb4f53a950
robl <robl@monkeyhelper.com>
2010-11-19 10:51:27 +0000

    Tidying layout

--- app/views/intentions/destroy.js
+++ app/views/intentions/destroy.js
@@ -1,4 +1,4 @@
 $('#broadcast<%= @intention.broadcast_id %>').attr('href', '<%= intentions_path(:broadcast_id => @intention.broadcast_id)%>');
 $('#broadcast<%= @intention.broadcast_id %>').attr('data-method', 'post');
-$('#broadcast<%= @intention.broadcast_id %> span.buttontext').text('Watch');
+//$('#broadcast<%= @intention.broadcast_id %> span.buttontext').text('Watch');
 $('#broadcast<%= @intention.broadcast_id %>').removeClass('watching');
