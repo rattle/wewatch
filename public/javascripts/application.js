9162444c157765e727f096c0ee10fe2ea7f483db
robl <robl@monkeyhelper.com>
2010-11-19 20:17:48 +0000

    Adding carousel

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,6 +2,11 @@ $(document).ready(function() {
 
   $('#dialog').jqm();
   $('#watchers').jqm();
+  $('#7pm').tinycarousel({ display: 6 });
+  $('#8pm').tinycarousel({ display: 6 });
+  $('#9pm').tinycarousel({ display: 6 });
+  $('#10pm').tinycarousel({ display: 6 });
+  $('#11pm').tinycarousel({ display: 6 });
 
 });
 
