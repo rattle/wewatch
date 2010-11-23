b7a93fe43ef87722117d7e1f01056a4247ff98d1
robl <robl@monkeyhelper.com>
2010-11-23 11:50:09 +0000

    Fixing friends detection and tidy twitter message

--- app/models/broadcast.rb
+++ app/models/broadcast.rb
@@ -6,11 +6,10 @@ class Broadcast < ActiveRecord::Base
     scope :significant, :conditions => ['is_repeat = ? AND title NOT LIKE "%News%" AND title NOT LIKE "%Weather%"', false]
     scope :by_most_popular, :order => "intentions_count DESC"
     
+
    def friends(user)
-     users = []
      in_sql = user.friends.collect { |f| f.id }.join(',')
-     users << friend if self.intentions.count(:conditions => ['user_id IN (?) AND broadcast_id = ?', in_sql, self.id])>0
-     users
+     User.find(:all, :include => :intentions, :conditions => ["intentions.user_id IN (#{in_sql}) AND intentions.broadcast_id = ?", self.id])
    end
 
 end
