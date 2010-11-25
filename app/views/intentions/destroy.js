aef016ef77b62d844f1d7dec4f3646c8d18f0347
Andrew Pendrick <andrew@rattlecentral.com>
2010-11-25 21:51:10 +0000

    list item height changes and beta tag addition

--- app/views/intentions/destroy.js
+++ app/views/intentions/destroy.js
@@ -1,10 +1,8 @@
 // These are set to match the carousel added properties
 w = $('li#broadcast<%= @broadcast.id %>').width();
-h = $('li#broadcast<%= @broadcast.id %>').height();
 
 $('#broadcast<%= @broadcast.id %>').replaceWith('<%= escape_javascript(render(:partial => "/pages/broadcast", :locals => {:broadcast => @broadcast})) %>');
 $('li#broadcast<%= @broadcast.id %>').css('overflow', 'visible');
 $('li#broadcast<%= @broadcast.id %>').css('float', 'left');
 $('li#broadcast<%= @broadcast.id %>').width(w);
-$('li#broadcast<%= @broadcast.id %>').height(h);
 
