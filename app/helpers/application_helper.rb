45083e824a3a98dc982a4a2e086883b16d049b98
Frankie Roberto <frankie@frankieroberto.com>
2010-11-29 10:23:09 +0000

    Adding a new prevent-widow feature

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,5 +21,25 @@ module ApplicationHelper
     end
   end
   
+  def prevent_widows(text)
+    words = text.split(" ")
+    if words.size >= 4
+      wc = words.size
+      t = ""
+      words.each_with_index do |word, i|
+        if (i+1) == words.size
+          t += word.html_safe
+        elsif (i+1) == (words.size - 1)
+          t += word.html_safe + "&#160;".html_safe
+        else
+          t += word.html_safe + " "
+        end
+      end
+      return t
+    else
+      text
+    end
+  end
+  
   
 end
