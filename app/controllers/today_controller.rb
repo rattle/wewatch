b9b5722e1ee6ae01d75864791c3c378d07206924
Frankie Roberto <frankie@frankieroberto.com>
2011-04-20 12:09:39 +0100

    Adding eager loading for a bit of speed

--- app/controllers/today_controller.rb
+++ app/controllers/today_controller.rb
@@ -5,7 +5,7 @@ class TodayController < ApplicationController
     start_time = DateTime.parse(@today.strftime("%Y-%m-%d") + "T19:00:00")
     end_time = DateTime.parse(@today.strftime("%Y-%m-%d") + "T23:59:59")
 
-    @broadcasts = Broadcast.where(:start => start_time..end_time).significant.order("start")
+    @broadcasts = Broadcast.where(:start => start_time..end_time).includes(:channel).significant.order("start")
     
     respond_to do |format|
       format.xml
