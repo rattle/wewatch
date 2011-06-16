e8dc433ff0c6106e7219f3e6bcdcdf32c0c5b1ea
Frankie Roberto <frankie@frankieroberto.com>
2011-06-16 15:55:22 +0100

    Simple watch stream on homepage

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -7,6 +7,8 @@ class HomepageController < ApplicationController
 
     @broadcasts = Broadcast.watchable.where(:start => start_time..end_time).includes(:channel)
 
+    @intentions = Intention.joins(:broadcast).includes(:user).where("broadcasts.start" => start_time..end_time).order("intentions.created_at desc").limit(20)
+
   end
 
 end
--- app/models/intention.rb
+++ app/models/intention.rb
@@ -1,10 +1,10 @@
 class Intention < ActiveRecord::Base
   belongs_to :broadcast, :counter_cache => true
   belongs_to :user, :counter_cache => true
-  
+
   validates_presence_of :broadcast, :user_id
   validates_uniqueness_of :user_id, :scope => :broadcast_id
-  
+
   validates_length_of :comment, :maximum => 80
-    
+
 end
