b58a35285bbb6eee6de697f948d88691cb63a070
Frankie Roberto <frankie@frankieroberto.com>
2011-06-23 12:30:43 +0100

    Ability to add broadcasts

--- app/controllers/broadcasts_controller.rb
+++ app/controllers/broadcasts_controller.rb
@@ -1,18 +1,36 @@
 class BroadcastsController < ApplicationController
 
   before_filter :authenticate_admin!, :except => :show
+  before_filter :find_broadcast, :only => [:show, :edit, :update, :destroy]
 
-  def show
-    @broadcast = Broadcast.find(params[:id])
-  end
-
-  def edit
-    @broadcast = Broadcast.find(params[:id])
+  def new
+    @broadcast = Broadcast.new
     @channels = Channel.all
   end
 
+  def edit
+    @channels = Channel.all
+  end
+
+  def create
+    @broadcast = Broadcast.new(params[:broadcast])
+
+    if @broadcast.save
+      redirect_to broadcast_path(@broadcast)
+    else
+      @channels = Channel.all
+      render :new
+    end
+
+  end
+
+  def destroy
+    @broadcast.destroy
+    redirect_to root_path
+  end
+
   def update
-    @broadcast = Broadcast.find(params[:id])
+
     if @broadcast.update_attributes(params[:broadcast])
       redirect_to broadcast_path(@broadcast)
     else
@@ -20,5 +38,11 @@ class BroadcastsController < ApplicationController
       render :edit
     end
   end
+
+  protected
+
+    def find_broadcast
+      @broadcast = Broadcast.find(params[:id])
+    end
 
 end
