43aed85272c2bd70e1d9830e74d103ae60adbca1
Frankie Roberto <frankie@frankieroberto.com>
2011-06-24 10:54:08 +0100

    Better ensuring referential integrity

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
   has_many :authorizations, :dependent => :destroy
   has_many :intentions, :dependent => :destroy
-  has_many :friendships
+  has_many :friendships, :dependent => :destroy
   has_many :friends, :through => :friendships
-  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id", :dependent => :destroy
   has_many :inverse_friends, :through => :inverse_friendships, :source => :user
 
   validates_presence_of :username
